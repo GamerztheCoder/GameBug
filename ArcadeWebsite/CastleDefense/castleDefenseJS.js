303d1553526cf2dc00311a5f117725913d9c459d
GamerztheCoder <111369464+GamerztheCoder@users.noreply.github.com>
2023-04-21 16:54:23 -0700

    Update castleDefenseJS.js

--- ArcadeWebsite/CastleDefense/castleDefenseJS.js
+++ ArcadeWebsite/CastleDefense/castleDefenseJS.js
@@ -12,26 +12,46 @@ constructor(x, y, width, height) {
   this.height = height;
 }
 
-render() {
+render(side) {
+  if(side == 'left'){
   ctx.fillStyle = "gray"; // Set fill color for the body
-  ctx.fillRect(this.x, this.y, this.width, this.height); // Draw the body
+  ctx.fillRect(this.x + 30, this.y, this.width - 60, this.height - 30); // Draw the body
 
   ctx.fillStyle = "black"; // Set fill color for the head
   ctx.beginPath();
-  ctx.arc(this.x + this.width/2, this.y - this.height/2, this.width/4, 0, Math.PI * 2); // Draw the head
+  ctx.arc(this.x + this.width/2, this.y - this.height/2 + 30, this.width/4, 0, Math.PI * 2); // Draw the head
   ctx.closePath();
   ctx.fill();
 
   ctx.fillStyle = "brown"; // Set fill color for the gun
-  ctx.fillRect(this.x - this.width/4, this.y + this.height/4, this.width/2, this.height/2); // Draw the gun
+  ctx.fillRect(this.x - this.width/4 + 30, this.y + this.height/4 - 15, this.width/2 - 30, this.height/2 - 30); // Draw the gun
 
   ctx.fillStyle = "black"; // Set fill color for the gun barrel
-  ctx.fillRect(this.x - this.width/4, this.y - this.height/2, this.width/2, this.height/8); // Draw the gun barrel
+  ctx.fillRect(this.x - this.width/4 + 10, this.y - this.height/2 + 53, this.width/2, this.height/8); // Draw the gun barrel
+  }
+  if(side=='right'){
+    ctx.fillStyle = "gray"; // Set fill color for the body
+  ctx.fillRect(this.x + 30, this.y, this.width - 60, this.height - 30); // Draw the body
+
+  ctx.fillStyle = "black"; // Set fill color for the head
+  ctx.beginPath();
+  ctx.arc(this.x + this.width/2, this.y - this.height/2 + 30, this.width/4, 0, Math.PI * 2); // Draw the head
+  ctx.closePath();
+  ctx.fill();
+
+  ctx.fillStyle = "brown"; // Set fill color for the gun
+  ctx.fillRect(this.x - this.width/4 + 80, this.y + this.height/4 - 15, this.width/2 - 30, this.height/2 - 30); // Draw the gun
+
+  ctx.fillStyle = "black"; // Set fill color for the gun barrel
+  ctx.fillRect(this.x - this.width/4 + 70, this.y - this.height/2 + 53, this.width/2, this.height/8); // Draw the gun barrel
+  }
 }
 }
 
 // Create a Gunman instance
-const gunman = new Gunman(250, 400, 100, 150);
+const gunman1 = new Gunman(150, 200, 80, 100);
+const gunman2 = new Gunman(400, 200, 80, 100);
 
 // Render the gunman on the canvas
-gunman.render();
+gunman1.render('left');
+gunman2.render('right');
