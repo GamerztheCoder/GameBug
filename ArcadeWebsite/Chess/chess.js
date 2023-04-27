a2594451791be37b41bbc106a84db6c201f1e10f
GamerztheCoder <111369464+GamerztheCoder@users.noreply.github.com>
2023-04-27 07:59:34 -0700

    Update chess.js

--- ArcadeWebsite/Chess/chess.js
+++ ArcadeWebsite/Chess/chess.js
@@ -2,6 +2,7 @@ const kingWhite = document.querySelector('.kingWhite');
 const pawnWhite = document.querySelector('.pawnWhite')
 console.log(pawnWhite)
 pawnWhite.addEventListener('mousedown', function(event){
+  
   document.getElementById('a3').classList.add('pawnWhite');
   document.getElementById('a2').classList.remove('pawnWhite');
 });
