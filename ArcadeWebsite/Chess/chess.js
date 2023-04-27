003dc87d28cb88af7126f901d5806f5565b560a4
GamerztheCoder <111369464+GamerztheCoder@users.noreply.github.com>
2023-04-27 09:20:40 -0700

    Update chess.js

--- ArcadeWebsite/Chess/chess.js
+++ ArcadeWebsite/Chess/chess.js
@@ -1,20 +1,41 @@
 const kingWhite = document.querySelector('.kingWhite');
 const pawnWhite = document.querySelectorAll('.pawnWhite');
+const pawnBlack = document.querySelectorAll('.pawnBlack')
 const chessBoard = document.querySelector('.board');
-console.log(chessBoard)
 
-chessBoard.addEventListener('click', function(event){
-  console.log(event);
-})
+function handlePawnClickBlack(pawnevent) {
+  function handleChessBoardClick(event) {
+    document.getElementById(event.target.id).classList.add('pawnBlack');
+    document.getElementById(pawnevent.target.id).classList.remove('pawnBlack');
+  }
 
+  chessBoard.addEventListener('click', handleChessBoardClick);
+
+  setTimeout(function() {
+    chessBoard.removeEventListener('click', handleChessBoardClick);
+  }, 1000);
+}
+function handlePawnClickWhite(pawnevent) {
+  function handleChessBoardClick(event) {
+    document.getElementById(event.target.id).classList.add('pawnWhite');
+    document.getElementById(pawnevent.target.id).classList.remove('pawnWhite');
+  }
+
+  chessBoard.addEventListener('click', handleChessBoardClick);
+
+  setTimeout(function() {
+    chessBoard.removeEventListener('click', handleChessBoardClick);
+  }, 1000);
+}
+
+pawnBlack.forEach(pawn => {
+  pawn.addEventListener('click', handlePawnClickBlack);
+});
 pawnWhite.forEach(pawn => {
-  pawn.addEventListener('click', function(pawnevent){
-    chessBoard.addEventListener('click', function(event){
-      document.getElementById(event.target.id).classList.add('pawnWhite');
-      document.getElementById(pawnevent.target.id).classList.remove('pawnWhite');
-    })
-  })
+  pawn.addEventListener('click', handlePawnClickWhite);
 })
+
+
 
 /* pawnWhite.addEventListener('mousedown', function(event){
   document.getElementById('a3').classList.add('pawnWhite');
